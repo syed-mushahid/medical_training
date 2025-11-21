3ed44353f7df8c64adfe04f648e7158bf2452e20
syed-mushahid <syedmk600@gmail.com>
2025-11-21 22:25:20 +0100

    updated api url

--- frontend/src/pages/ChatMessages.jsx
+++ frontend/src/pages/ChatMessages.jsx
@@ -43,7 +43,7 @@ export default function ChatMessages() {
   
   // Get API URL from environment or use default
   const getApiUrl = () => {
-    return 'http://localhost:5000';
+    return 'http://46.224.35.114:5000';
   };
 
   useEffect(() => {
